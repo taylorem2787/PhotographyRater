549e0e3ff3dc5be22aa18e11748ae8708c775eec
kulini <conflicted@gmail.com>
2016-11-12 10:38:07 -0500

    test

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE photos
+(
+id int NOT NULL AUTO_INCREMENT,
+wish varchar(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+INSERT INTO photos (wish) VALUES ('Shaan wants to read minds.');
+INSERT INTO photos (wish) VALUES ('John wins the lottery.');
+INSERT INTO photos (wish) VALUES ('Kelly wishes for a room full of kittens.');
