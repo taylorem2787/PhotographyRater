a50e99a94c18988c3f55a7cd6aee80243c2cd2c7
kulini <conflicted@gmail.com>
2016-11-12 12:25:50 -0500

    ch ch ch changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,11 @@
 CREATE TABLE photos
 (
 id int NOT NULL AUTO_INCREMENT,
-wish varchar(255) NOT NULL,
+url varchar(255) NOT NULL,
+bw boolean	NOT NULL default 0, 	/* black and white has value of 1. zero otherwise */
+filter boolean NOT NULL default 0 	/* was a filter used for the photo? if so, value of 1. default value of zero */
+
+
 PRIMARY KEY (id)
 );
 
