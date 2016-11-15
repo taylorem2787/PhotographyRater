6b6d2e1085fe8dbc673bc2472dff7d2c9faa8f5e
Taylor Emolo <tayloremolo@tay.local>
2016-11-15 16:05:14 -0500

    Photos

--- app/app.js
+++ app/app.js
@@ -1,3 +1,19 @@
+
+
+//PARALLAX=======================================================================
 $(document).ready(function(){
 	$('.parallax').parallax();
-});
+
+//MODAL TRIGGER==================================================================
+$('.modal').modal();
+$('.modal-trigger').leanModal({
+  dismissible: true, // Modal can be dismissed by clicking outside of the modal
+  opacity: .5, // Opacity of modal background
+  in_duration: 300, // Transition in duration
+  out_duration: 200, // Transition out duration
+  starting_top: '4%', // Starting top style attribute
+  ending_top: '10%', // Ending top style attribute
+  }
+); //END MATERIALIZE
+$('select').material_select();
+}); //END DOC READY
--- server.js
+++ server.js
@@ -60,3 +60,4 @@ app.get('/', function(req,res) {
 
 
 // });
+
