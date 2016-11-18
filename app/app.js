b3ebb1cdcf5bd7dd11bf7ade35c0779d042dd48d
Taylor Emolo <tayloremolo@tay.local>
2016-11-18 09:20:39 -0500

    Profile changes

--- app/app.js
+++ app/app.js
@@ -1,8 +1,75 @@
+<<<<<<< Updated upstream
 
 
 //PARALLAX=======================================================================
 $(document).ready(function(){
 	$('.parallax').parallax();
+=======
+$(document).ready(function(){
+
+	// Initializing materialize parallax display
+	$('.parallax').parallax();
+
+
+	// EVENT LISTENERS: 
+	// Displays pop-over for username and password
+	$('#login').webuiPopover({url:'#login-form'});
+
+
+
+}); // End of document.ready function
+
+
+//GENERAL FUNCTIONS: ==============================================================================================
+
+// General function for opening modals
+function openDisplay(id) {
+
+	switch(id) {
+
+		case 'registration':
+			console.log("opening registration modal");
+			$('#registration-modal').openModal();
+
+			// After registration modal is closed, run calibration function
+			// $('#registration-modal').on("click", ".close-registration", function() {
+			// 	openDisplay("calibration");
+			// })
+			// break;
+
+        // case 'calibration':
+        //     $('#calibration').openModal();
+        //     console.log("opening calibration modal");
+        //     break;
+
+		default:
+			console.log('oops!');
+	}
+}
+
+
+//CALLING FUNCTIONS: ==============================================================================================
+        // $("#exploreBtn").click( function()
+        //    {
+        //      alert('button clicked');
+        //    }
+        // );
+    // $('#exploreBtn').click( function() {
+    // 	 alert('button clicked');
+    //     var randomImg = $(this).data('image');
+    //     var queryURL = "";
+
+    //     $.ajax({
+    //             url: queryURL,
+    //             method: 'GET'
+    //         })
+    //         .done(function(response) {
+    //           
+
+    //             console.log(response)
+
+    //               var results = response.data;
+>>>>>>> Stashed changes
 
 //MODAL TRIGGER==================================================================
 $('.modal').modal();
