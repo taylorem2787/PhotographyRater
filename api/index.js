ec003ec76eccecdebd678ac8f37866b52e92f5a3
kulini <conflicted@gmail.com>
2016-11-22 23:29:15 -0500

    fixed conflict in index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 var nextImage = require('./nextImagecopy.js')
-=======
-var nextImage = require('./nextImageCopy')
->>>>>>> 1834130d9296f5bbd3ce9e7150417754ebdde26a
 
 var api = {
 	'nextImage': nextImage
